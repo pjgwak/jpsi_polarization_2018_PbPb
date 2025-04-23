6dd43eaf2bc1bacc756c2cddfb32cb66aa1e24eb
pjgwak <piljun.gwak@gmail.com>
2025-04-23 21:09:14 +0900

    eff study - run2 PbPb

---
+++ archive/eff_PbPb_250418/script_eff.sh
@@ -0,0 +1,10 @@
+# # ====== only for initial setting ===== #
+# rm -rf figs
+# mkdir figs figs/eff_fwd figs/eff_mid
+
+# rm -rf roots
+# mkdir roots
+# # ====== #
+
+# root -l -b -q eff_pp.C
+# python3 draw_eff_pp.py
